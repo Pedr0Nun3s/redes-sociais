4f8b8b5186e69a846b33f53d37a1e8a4a528f556
Pedro nunes <111570874+Pedr0Nun3s@users.noreply.github.com>
2024-11-29 10:38:39 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,12 @@
+const getcss = (variavel) => {
+  const bodyStyles = getComputedstyle(document.body)
+  return bodystyles.getPropertyValue(variavel)
+}
 
+const tickconfig = }
+  family: getcss("--font C
+  size: 16,
+  color: getcss("--primary-color')
+{
+
+export {getcss, tickconfig}
